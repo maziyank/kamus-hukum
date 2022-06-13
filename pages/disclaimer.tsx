b2b599226ee5fcc85a9d7bb529dbbf7a789875c5
maziyank <maziyank@gmail.com>
2022-06-13 13:54:02 +1000

    update page

--- pages/disclaimer.tsx
+++ pages/disclaimer.tsx
@@ -11,6 +11,9 @@ const Disclaimer = () => {
                     <li>
                         Penggunaan definisi hukum dari aplikasi ini adalah tanggung jawab pengguna. Meski kami membantu untuk menyediakan data namun pengguna tetap wajib untuk melakukan validasi dengan dokumen aslinya. Hal ini mengingat semua definisi hukum yang ada pada database kami diperoleh dengan cara melakukan parsing terhadap dokumen Undang-Undang yang kami kumpulkan. Proses parsing dilakukan secara otomatis sehingga mungkin terjadi kesalahan ketik. Selain itu, beberapa definisi mungkin berasal dari Undang-Undang yang sudah tidak berlaku.
                     </li>
+                    <li>
+                        Pengguna mungkin akan menemui satu definisi dengan penjelasan yang berbeda. Hal ini terjadi karena pada dasarnya definisi bisa dibatasi konteks yang dinyatakan dalam setiap regulasi pada Ketentuan Umum, sebagai contoh dalam Undang-Undang ini yang dimaksud dengan: ....
+                    </li>
                 </ol></p>
             </div>
         </div>
