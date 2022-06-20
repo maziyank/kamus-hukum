99718857c9dda5a486002d0b0bf2f12f2ef699af
maziyank <maziyank@gmail.com>
2022-06-20 16:18:52 +1000

    update style

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,12 +5,18 @@ const About = () => {
 
     return (
         <div className="flex w-full bg-slate-100 min-h-screen h-auto justify-center">
-            <div className="max-w-lg w-full p-10 bg-slate-300">
-                <h1 className="max-w-lg text-xl font-bold font-serif mb-5">Tentang Aplikasi</h1>
-                <div className="w-full">
+
+            <div className="max-w-lg w-full bg-slate-300">
+                <nav className="flex items-center justify-between flex-wrap bg-blue-900 p-3 shadow-lg">
+                    <div className="flex items-center flex-shrink-0 text-white mr-6">
+                        <span className="font-semibold text-xl tracking-tight">Tentang Aplikasi</span>
+                    </div>
+                </nav>
+
+                <div className="w-full p-8">
                     <p className="mb-3" >Aplikasi Kamus Hukum memuat kumpulan definisi hukum yang diambil dari peraturan perundang-undangan yang ada di Indonesia. Aplikasi ini dapat dimanfaatkan oleh praktisi hukum, akademisi atau masyarakat yang memerlukan cara cepat untuk mencari definisi dari sebuah terminologi hukum secara cepat.</p>
                     <p className="mb-3">Saat ini, fitur aplikasi dan database masih dalam proses pengembangan. Kami berkomitmen untuk terus memperbaharui fitur aplikasi dan daftar terminologi hukum di database kami agar semakin dapat memberikan nilai tambah kepada pengguna.</p>
-                    <p>Kontributor Kamus Hukum:</p>
+                    <p className='font-bold'>Kontributor Kamus Hukum:</p>
                     <ol className="pl-4 list-decimal">
                         <li>
                             Bakhtiar Amaludin
@@ -31,7 +37,7 @@ const About = () => {
 
                 </div>
                 <div className="w-full mt-5 flex justify-center">
-                    <button className="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-red-/600" onClick={e => router.back()}>Kembali</button>
+                    <button className="h-10 w-20 text-white rounded-lg bg-blue-700 hover:bg-red-/600" onClick={e => router.back()}>Kembali</button>
 
                 </div>
             </div>
--- pages/disclaimer.tsx
+++ pages/disclaimer.tsx
@@ -5,25 +5,31 @@ export default function Disclaimer() {
 
     return (
         <div className="flex w-full bg-slate-100 min-h-screen h-auto justify-center">
-            <div className="max-w-lg w-full p-10 bg-slate-300">
-                <h1 className="max-w-lg text-xl font-bold font-serif mb-5">Disclaimer</h1>
-                <div className="w-full">
+            <div className="max-w-lg w-full bg-slate-300">
+                <nav className="flex items-center justify-between flex-wrap bg-blue-900 p-3 shadow-lg">
+                    <div className="flex items-center flex-shrink-0 text-white mr-6">
+                        <span className="font-semibold text-xl tracking-tight">Disclaimer</span>
+                    </div>
+                </nav>
+
+
+                <div className="w-full p-8">
                     <p> Harap membaca dan memahai prasyarat berikut dengan seksama:</p>
                     <ol className="list-decimal pl-5 mt-2">
                         <li className="mt-2">
                             Aplikasi ini tidak terafiliasi dengan instansi manapun, karena dikembangkan atas nama pribadi untuk membantu para pihak yang membutuhkan.
                         </li>
                         <li className="mt-2">
-                            Penggunaan definisi hukum dari aplikasi ini adalah tanggung jawab pengguna. Aplikasi ini hanya membantu untuk menyediakan data, sehingga pengguna tetap wajib untuk melakukan validasi dengan dokumen aslinya. Hal ini mengingat semua definisi hukum yang ada pada database kami diperoleh dengan cara melakukan <i>parsing</i> terhadap dokumen Undang-Undang yang kami kumpulkan yang sebagian besar berformat PDF. Proses <i>parsing</i> terhadap dokumen PDF memungkinkan sekali kami tidak dapat menangkap teks secara utuh. 
+                            Penggunaan definisi hukum dari aplikasi ini adalah tanggung jawab pengguna. Aplikasi ini hanya membantu untuk menyediakan data, sehingga <b>pengguna tetap wajib untuk melakukan validasi dengan dokumen aslinya</b>. Hal ini mengingat semua definisi hukum yang ada pada database kami diperoleh dengan cara melakukan <i>parsing</i> terhadap dokumen Undang-Undang yang kami kumpulkan yang sebagian besar berformat PDF. Proses <i>parsing</i> terhadap dokumen PDF memungkinkan sekali kami tidak dapat menangkap teks secara utuh.
                         </li>
                         <li className="mt-2">
-                        Beberapa definisi mungkin berasal dari Undang-Undang yang sudah tidak berlaku. Selain itu, pengguna mungkin akan menemui satu definisi di dua atau lebih peraturan yang masih berlaku dengan penjelasan yang berbeda. Hal ini dapat terjadi karena penggunaan definisi bisa dibatasi konteks yang dinyatakan dalam setiap regulasi pada Ketentuan Umum.
+                            Beberapa definisi mungkin berasal dari Undang-Undang yang sudah tidak berlaku. Selain itu, pengguna mungkin akan menemui satu definisi di dua atau lebih peraturan yang masih berlaku dengan penjelasan yang berbeda. Hal ini dapat terjadi karena penggunaan definisi bisa dibatasi konteks yang dinyatakan dalam setiap regulasi pada Ketentuan Umum.
                         </li>
                     </ol>
 
                 </div>
                 <div className="w-full mt-5 flex justify-center">
-                    <button className="h-10 w-20 text-white rounded-lg bg-blue-500 hover:bg-red-/600" onClick={e => router.back()}>Kembali</button>
+                    <button className="h-10 w-20 text-white rounded-lg bg-blue-800 hover:bg-red-/600" onClick={e => router.back()}>Kembali</button>
                 </div>
             </div>
         </div>
