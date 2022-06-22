5b8737c4046ece123212f029b872508f6dd3609e
Sukirno <mblonyox@gmail.com>
2022-06-22 13:49:49 +0700

    fix: add title & meta description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "../components/layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Kamus Hukum</title>
+        <meta name="description">Kamus Hukum Indonesia</meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
