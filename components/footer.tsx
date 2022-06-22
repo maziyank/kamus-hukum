6db93a1e6848293994cf8076e3e304eea939f616
Sukirno <mblonyox@gmail.com>
2022-06-22 13:58:14 +0700

    fix: links in footer not crawlable

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,13 +4,13 @@ export default function Footer() {
   return (
     <div className="fixed bottom-0 max-w-lg w-full flex justify-center bg-blue-800 p-3 text-white shadow-lg">
       <Link href="/about">
-        <a className="mx-2 font-semibold hover:text-blue-300" href="">
+        <a className="mx-2 font-semibold hover:text-blue-300">
           <span>Tentang</span>
         </a>
       </Link>
       |
       <Link href="/disclaimer">
-        <a className="mx-2 font-semibold hover:text-blue-300" href="">
+        <a className="mx-2 font-semibold hover:text-blue-300">
           <span>Disclaimer</span>
         </a>
       </Link>
