25204e10d1910fc55edc77ab0e87878ab47176ae
Sukirno <sukirno89@kemenkeu.go.id>
2022-06-24 04:50:57 +0700

    fix: layout, sticky navbar, no fixed footer

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 max-w-lg z-50 w-full flex items-center justify-between flex-wrap bg-gradient-to-r to-cyan-600 from-blue-800 p-3 shadow-lg">
+    <nav className="sticky top-0 z-50 w-full flex items-center justify-between flex-wrap bg-gradient-to-r to-cyan-600 from-blue-800 p-3 shadow-lg">
       <div className="flex items-center flex-shrink-0 text-white mr-6">
         <span className="font-semibold text-xl tracking-tight">
           Kamus Hukum Indonesia
