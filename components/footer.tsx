2965feb4594ba0f56f0ec5b4674c1d7103ba670a
maziyank <maziyank@gmail.com>
2022-06-23 23:23:26 +1000

    add gradient to navbar and footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="fixed bottom-0 max-w-lg w-full flex justify-center bg-blue-800 p-3 text-white shadow-lg">
+    <div className="fixed bottom-0 max-w-lg w-full flex justify-center bg-gradient-to-r to-cyan-600 from-blue-800 p-3 text-white shadow-lg">
       <Link href="/about">
         <a className="mx-2 font-semibold hover:text-blue-300">
           <span>Tentang</span>
