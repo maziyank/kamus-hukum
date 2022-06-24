55462a3f646d657e63acd772169f3427c21864df
maziyank <maziyank@gmail.com>
2022-06-24 12:07:55 +1000

    fix SVG

--- components/copyToClipboardButton.tsx
+++ components/copyToClipboardButton.tsx
@@ -13,13 +13,16 @@ export default function CopyToClipboardButton({
   }, [data]);
 
   return (
-    <button
-      type="button"
-      className="inline-block p-2 bg-slate-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-slate-700 hover:shadow-lg"
-      aria-label="Copy to clipboard"
-      onClick={onButtonClick}
-    >
-      <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg>
-    </button>
+    <div className='has-tooltip'>
+      <span className='tooltip rounded shadow-lg p-1 bg-gray-800 opacity-80 -mt-8 -ml-4 text-white text-xs'>Salin Teks</span>
+      <button
+        type="button"
+        className="inline-block p-2 bg-slate-400 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-slate-700 hover:shadow-lg"
+        aria-label="Copy to clipboard"
+        onClick={onButtonClick}
+      >
+        <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path></svg>
+      </button>
+    </div>
   );
 }
