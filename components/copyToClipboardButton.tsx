8fdf0a4a7117cca129b85c9dcb8a9f85076c6aac
maziyank <maziyank@gmail.com>
2022-06-24 13:47:09 +1000

    fix svg

--- components/copyToClipboardButton.tsx
+++ components/copyToClipboardButton.tsx
@@ -29,7 +29,7 @@ export default function CopyToClipboardButton({
       >
         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
           {!copied && <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"></path>}
-          {copied && <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7" />}
+          {copied && <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />}
         </svg>
       </button>
     </div>
