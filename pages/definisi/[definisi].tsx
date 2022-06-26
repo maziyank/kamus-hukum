30210587f2363b8036e790e343cfb1957ede386f
maziyank <maziyank@gmail.com>
2022-06-26 19:18:34 +1000

    sort similaritu list

--- pages/definisi/[definisi].tsx
+++ pages/definisi/[definisi].tsx
@@ -11,7 +11,7 @@ export const getServerSideProps: GetServerSideProps<DefinisiPageProps> = async (
   const xcAuth = process.env.API_XC_AUTH;
 
   const url = `${baseUrl}/Kamus?sort=-Tahun,-No&where=(Definisi,eq,${definisi})`;
-  const urlMirip = `${baseUrl}/Kemiripan?sort=-Tahun,-No&where=(Def1,eq,${CurrentId})`;
+  const urlMirip = `${baseUrl}/Kemiripan?sort=-Score&where=(Def1,eq,${CurrentId})`;
 
   const { list: data, pageInfo } = await fetch(url, {
     headers: { "xc-auth": xcAuth },
@@ -23,12 +23,11 @@ export const getServerSideProps: GetServerSideProps<DefinisiPageProps> = async (
 
 
   const excludeId = data.map(({ Id }, i) => Id);
-  const listMiripMap = new Map(listMirip.filter(({ Def2 }) => !excludeId.includes(Def2) && Def2 !== CurrentId)
+  const listMiripScore = new Map(listMirip.filter(({ Def2 }) => !excludeId.includes(Def2) && Def2 !== CurrentId)
     .map(({ Def2: Id, Score }) => { return [Id, Score] }));
 
-
-  const listMiripId = Array.from(listMiripMap.keys()).join(',')
-  const urlMiripData = `${baseUrl}/Kamus?sort=-Tahun,-No&where=(Id,in,${listMiripId})`;
+  const listMiripId = Array.from(listMiripScore.keys()).join(',')
+  const urlMiripData = `${baseUrl}/Kamus?where=(Id,in,${listMiripId})`;
 
   const { list: miripData } = await fetch(urlMiripData, {
     headers: { "xc-auth": xcAuth },
@@ -40,20 +39,19 @@ export const getServerSideProps: GetServerSideProps<DefinisiPageProps> = async (
       definisi,
       data,
       pageInfo,
-      listMiripMap: Object.fromEntries(listMiripMap),
+      listMiripScore: Object.fromEntries(listMiripScore),
       miripData
     },
     notFound: !data.length,
   };
 };
 
-
 export interface DefinisiPageProps {
   CurrentId: number;
   definisi: string;
   data: KamusItem[];
   miripData: KamusItem[];
-  listMiripMap: any;
+  listMiripScore: any;
   pageInfo: PageInfo;
 }
 
@@ -63,7 +61,7 @@ export default function DefinisiPage({
   data,
   pageInfo,
   miripData,
-  listMiripMap
+  listMiripScore
 }: DefinisiPageProps) {
 
   return (
@@ -75,7 +73,7 @@ export default function DefinisiPage({
           .map(({ Id, Url, Sumber, Verified, Keterangan }, i) => (
             <div key={Id} className="my-2 border-t border-gray-300">
               <p className="text-gray-700 text-base mt-2">{Keterangan}.</p>
-              <p className="text-black text-sm bg-gray-200 rounded-md p-2 my-2 hover:bg-blue-300 cursor-pointer">
+              <p className="text-black text-sm bg-gray-200 rounded-md p-2 my-2 cursor-pointer">
                 Sumber:{" "}
                 <a
                   href={Url}
@@ -84,6 +82,9 @@ export default function DefinisiPage({
                 >
                   {Sumber}
                 </a>
+              </p>
+              <p className="text-black text-sm bg-gray-200 rounded-md p-2 my-2 cursor-pointer">
+                Status: {Verified ? " Sudah diverifikasi" : " Belum diverifikasi"}
               </p>
             </div>
           ))}
@@ -101,10 +102,31 @@ export default function DefinisiPage({
             .map(({ Id, Url, Definisi, Sumber, Verified, Keterangan }, i) => (
               <li className="mb-10 ml-4" key={Id}>
                 <div className="absolute w-8 h-8 bg-slate-200 rounded-full -left-4 border border-white dark:border-slate-900 dark:bg-slate-700 text-slate-700 flex justify-center items-center text-sm">{i + 1}</div>
-                <div className="ml-2 mb-1 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
-                  <a href={Url} target="_blank" rel="noreferrer">
+                <div className="flex flex-row justify-between ml-2 mb-1 text-sm font-normal leading-none text-slate-400 dark:text-slate-500">
+                  <a href={Url} className="pt-2" target="_blank" rel="noreferrer">
                     <span className="pt-2 cursor-pointer">{Sumber}</span>
                   </a>
+                  {!Verified && (
+                    <div className="has-tooltip">
+                      <span className="tooltip rounded shadow-lg p-1 bg-gray-800 opacity-80 -mt-8 -ml-8 text-white text-xs">
+                        Terverifikasi
+                      </span>
+                      <span className="bg-blue-100 h-6 w-6 mt-2 text-blue-800 text-sm font-semibold inline-flex items-center p-1 rounded-full dark:bg-blue-200 dark:text-blue-800 cursor-pointer">
+                        <svg
+                          className="w-3.5 h-3.5"
+                          fill="currentColor"
+                          viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                            clipRule="evenodd"
+                          ></path>
+                        </svg>
+                      </span>
+                    </div>
+                  )}
                 </div>
                 <Link
                   href={{
@@ -122,26 +144,50 @@ export default function DefinisiPage({
 
       <div className="w-full block rounded-lg shadow-lg bg-white text-left p-6 mt-4">
         <p>
-          Daftar definisi lain dengan penjelasan yang mirip.
+          Definisi lain dengan penjelasan yang mirip.
         </p>
         <div className="border-b border-gray-300 pt-2"></div>
         <ol className="relative border-l border-slate-200 dark:border-slate-700 mt-5 ml-4">
           {miripData
             .filter(({ Id }, i) => CurrentId !== Id)
+            .sort((a, b) => listMiripScore[b.Id] - listMiripScore[a.Id])
             .map(({ Id, Url, Definisi, Sumber, Verified, Keterangan }, i) => (
               <li className="mb-10 ml-4" key={Id}>
                 <div className="absolute w-8 h-8 bg-slate-200 rounded-full -left-4 border border-white dark:border-slate-900 dark:bg-slate-700 text-slate-700 flex justify-center items-center text-sm">{i + 1}</div>
-                <div className="ml-2 mb-1 text-sm font-normal leading-none text-slate-400 dark:text-slate-500 flex flex-row justify-between ">
-                  <a href={Url} target="_blank" rel="noreferrer">
-                    <span className="pt-2 cursor-pointer">{Sumber}</span>
+                <div className="flex flex-row justify-between ml-2 mb-1 text-sm font-normal leading-none text-slate-400 dark:text-slate-500 flex flex-row">
+                  <a className="mt-2" href={Url} target="_blank" rel="noreferrer">
+                    <span className="cursor-pointer">{Sumber}</span>
                   </a>
-                  <div className="has-tooltip">
-                    <span className="tooltip rounded shadow-lg p-1 bg-gray-800 opacity-80 -mt-7 text-white text-xs text-center">
-                      {(listMiripMap[Id] * 100).toFixed(2) + '% Mirip'}
-                    </span>
-                    <span className="text-xs inline-block py-1 px-2 rounded bg-blue-100 text-blue-400 uppercase ml-2 cursor-pointer">
-                      {(listMiripMap[Id] * 100).toFixed(2) + ' %'}
-                    </span>
+                  <div className="flex flex-row">
+                    {Verified && (
+                      <div className="has-tooltip">
+                        <span className="tooltip rounded shadow-lg p-1 bg-gray-800 opacity-80 -mt-8 -ml-8 text-white text-xs">
+                          Terverifikasi
+                        </span>
+                        <span className="bg-blue-100 h-6 w-6 text-blue-800 text-sm font-semibold inline-flex items-center p-1 rounded-full dark:bg-blue-200 dark:text-blue-800 cursor-pointer">
+                          <svg
+                            className="w-3.5 h-3.5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            ></path>
+                          </svg>
+                        </span>
+                      </div>
+                    )}
+                    <div className="has-tooltip">
+                      <span className="tooltip rounded shadow-lg p-1 bg-gray-800 opacity-80 -mt-7 text-white text-xs text-center">
+                        {(listMiripScore[Id] * 100).toFixed(2) + '% Mirip'}
+                      </span>
+                      <span className="text-xs inline-block py-1 px-2 rounded bg-blue-100 text-blue-400 uppercase ml-2 cursor-pointer">
+                        {(listMiripScore[Id] * 100).toFixed(2) + ' %'}
+                      </span>
+                    </div>
                   </div>
                 </div>
                 <Link
